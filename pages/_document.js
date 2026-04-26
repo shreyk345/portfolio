9f24844f606172b828ddbb56ad7eaecf3e1e8062
Shreya Krishnamurthy <shreya@Shreyas-MacBook-Air.local>
2026-04-25 17:18:34 -0700

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="icon" type="image/svg+xml" href="/images/letter-blocks/k.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Reenie+Beanie&family=Lato:wght@300;400;700&display=swap"
           rel="stylesheet"
